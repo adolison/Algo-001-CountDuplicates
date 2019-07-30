d06d24361ae0fb7e2ab2482fedcb8519b0d9c1ad
adolison <amdol1@morgan.edu>
2019-07-30 05:44:22 -0400

    int and string duplicates counted

--- src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
+++ src/main/java/hbcu/stay/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,8 @@
 package hbcu.stay.ready.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +10,23 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        Map<String,Integer> map = new HashMap<>();
+        Integer duplicates =0;
+        for (int i = 0;i<input.length;i++){
+            if (!map.containsKey(input[i])){
+                map.put(input[i],1);
+            }else{
+                Integer keyValue = map.get(input[i]);
+                if (keyValue==1){
+                    duplicates++;
+                }
+                keyValue++;
+                map.put(input[i],keyValue);
+
+
+            }
+        }
+        return duplicates;
     }
 
     /**
@@ -16,6 +35,43 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+
+       Map<Integer,Integer> map = new HashMap<>();
+       Integer duplicates =0;
+       for (int i = 0;i<input.length;i++){
+           if (!map.containsKey(input[i])){
+               map.put(input[i],1);
+           }else{
+               Integer keyValue = map.get(input[i]);
+               if (keyValue==1){
+                   duplicates++;
+               }
+               keyValue++;
+               map.put(input[i],keyValue);
+
+
+           }
+       }
+        /*Integer duplicates = 0;
+        Integer holdNum =0;
+        for( int firstInt=0; firstInt<input.length;firstInt++) {
+            for (int compareTo = firstInt+1; compareTo < input.length; compareTo++) {
+                if (input[firstInt] == input[compareTo]) {
+                    if (holdNum !=1) {
+                        duplicates = duplicates + 1;
+
+                        holdNum = input[firstInt];
+                        break;
+                    }else
+                        if((input[firstInt] == input[compareTo])&&input[firstInt]>1) {
+                         duplicates = duplicates + 1;
+
+
+                    }
+                }
+            }
+        }*/
+
+        return duplicates;
     }
 }
